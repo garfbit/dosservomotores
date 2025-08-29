960bddc38b2d5e5624c9cd8ee513a03541f728f8
garfbit <104676660+garfbit@users.noreply.github.com>
2025-08-29 07:04:40 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+input.onButtonPressed(Button.A, function () {
+    pins.servoWritePin(AnalogPin.P1, 0)
+    basic.pause(2000)
+    pins.servoWritePin(AnalogPin.P1, 180)
+    basic.pause(2000)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.servoWritePin(AnalogPin.P2, 0)
+    basic.pause(2000)
+    pins.servoWritePin(AnalogPin.P2, 180)
+    basic.pause(2000)
+})
